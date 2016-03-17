6ae55c404401fe71212ab32db15e3a8f0e7af89f
JiaYan Hu <jiayan.a.hu@gmail.com>
2016-03-17 01:50:32 -0400

    added login and register page and styling

--- app/views.py
+++ app/views.py
@@ -49,3 +49,10 @@ def index():
     print games
     return render_template('index.html', games=games)
 
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+@app.route('/register/', methods=['GET', 'POST'])
+def register():
+    return render_template('register.html')
