88fd2d7b0dacbd86b4f98e855b0f81ab566435c1
kycwuwu <kcw2141@columbia.edu>
2016-03-17 00:05:04 -0400

    Oops wrong version A+

--- Plans and SQL Commands.sql
+++ Plans and SQL Commands.sql
@@ -7,5 +7,9 @@ SELECT * FROM Games;
 # Admins can only see Games that have NOT been already evaluated
 SELECT gameid, title, price FROM Games WHERE gameid NOT IN (SELECT gameid FROM Evaluate);
 
-# Gamers can only see Games that have been evaluated
-SELECT gameid, title, price FROM Games WHERE gameid IN (SELECT gameid FROM Evaluate);
+# Gamers/"store" can only see Games that have been evaluated; filter and sort as needed
+SELECT gameid, title, price FROM Games WHERE gameid IN (SELECT gameid FROM Evaluate) [AND os = "<input>"...etc][ORDER BY "<attr>" ASC/DESC];
+
+# Gamers/Library can only see what the Gamer owns
+SELECT gameid, title, genre, gameplay FROM Games, Library_Owned, Contains WHERE Games.gameid = Library_Owned.gameid
+AND Library_Owned.libraryid = Contains.libraryid AND gameid IN (SELECT gameid FROM Evaluate);
